01d7b15cea9982055be9506cf75e7f4aafee3a1a
BigZ5709 <89311149+BigZ5709@users.noreply.github.com>
2024-03-18 09:39:47 -0500

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,11 @@
+// function onClickCallback() {
+//     console.log("Thank you for your submission!");
+// }
+
+// let button = document.querySelector('button');
+
+// button.addEventListener('click', onClickCallback);
+
+// button.addEventListener('click', function(event) {
+//     //store who clicked it
+// }); 
